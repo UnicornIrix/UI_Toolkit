b0be292e34a5a0a2d1f5db7f1b3e089d6fc13b4f
UnicornIrix <iris.levrio@gmail.com>
2025-02-18 11:00:35 +0100

    non so che cazzo sto a fa, ma il timer funge

--- UItoolkit/Assets/Scripts/Timer.cs
+++ UItoolkit/Assets/Scripts/Timer.cs
@@ -8,15 +8,24 @@ public class Timer : VisualElement
 {
     private Label timer_label;
     public Timer(){
-        timer_label = new Label();
+        /*timer_label = new Label();
         timer_label.text = "00:00:00";  
         timer_label.style.fontSize = 50;
         timer_label.style.color = Color.white;
         Add(timer_label);
+        */ // avevamo creato il timer manualmente, ma adesso mi interessa usare un template
+
+        // prendi il file timer che contiene il mio file uxml e di cui è gia stato creato il visualtreeasset, caricalo in memoria e 
+        // clonalo all'interno di questo visualelement
+        var template = Resources.Load<VisualTreeAsset>("Timer_UXML");
+        template.CloneTree(this);
+
+        timer_label = this.Q<Label>();
+
 
         RegisterCallback<AttachToPanelEvent>(evt =>
         {
-            if(evt.destinationPanel.contextType == ContextType.Player && Application.isPlaying){ // non funge
+            if(evt.destinationPanel.contextType == ContextType.Player && Application.isPlaying){
                 Start();
             }
         });
